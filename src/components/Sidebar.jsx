052e08b6512160314c970daa0250ce4d98e5129c
acacia8628 <masashi.k.429@gmail.com>
2022-09-24 13:59:54 +0900

    new commit message

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -30,7 +30,7 @@ const Sidebar = () => {
               onClick={handleCloseSideBar}
               className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900"
             >
-              <SiShopware /> <span>Shoppy hoge huga wawawa</span><span>test</span>
+              <SiShopware /> <span>Shoppy test</span>
             </Link>
             <TooltipComponent content="Menu" position="BottomCenter">
               <button
